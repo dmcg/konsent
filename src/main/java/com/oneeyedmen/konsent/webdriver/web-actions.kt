d99091e5ddadd07e3a4c26a95c1e89813a735820
Duncan McGregor <duncan@oneeyedmen.com>
2016-05-10 17:11:35 +0100

    More example tests

--- src/main/java/com/oneeyedmen/konsent/webdriver/web-actions.kt
+++ src/main/java/com/oneeyedmen/konsent/webdriver/web-actions.kt
@@ -7,4 +7,7 @@ fun Steps<RemoteWebDriver>.loadsThePageAt(url: String) = describedBy("""loads th
     driver.get(url)
 }
 
+fun Steps<RemoteWebDriver>.followsTheLink(text: String, href: String) = describedBy(""""follows the link [$text]($href)""") {
+    driver.findElementByXPath("//a[@href='$href'][text()='$text']").click()
+}
 
--- src/test/java/com/oneeyedmen/konsent/KonsentExampleTests.kt
+++ src/test/java/com/oneeyedmen/konsent/KonsentExampleTests.kt
@@ -3,18 +3,34 @@ package com.oneeyedmen.konsent
 import com.natpryce.hamkrest.equalTo
 import com.oneeyedmen.konsent.webdriver.*
 import org.junit.Test
+import java.net.URI
 
 
-class KonsentExampleTests : ChromeAcceptanceTest() {
+class KonsentExampleTests : ChromeAcceptanceTest(null,
+    "As a developer named Duncan",
+    "I want to know that example.com is up and running") {
 
-    val james = actorNamed("James")
+    val duncan = actorNamed("Duncan")
 
     @Test fun `Example_dot_com loads`() {
-        given(james).loadsThePageAt("http://example.com")
-        then(james) {
+        given(duncan).loadsThePageAt("http://example.com")
+        then(duncan) {
             shouldSee(::`the page location`, pathContains("example.com"))
             shouldSee(::`the page title`, equalTo("Example Domain"))
             shouldSee(::`the page content`, containsALink("More information...", "http://www.iana.org/domains/example"))
         }
     }
+
+    @Test fun `Following a link from example_dot_com`() {
+        given(duncan).loadsThePageAt("http://example.com")
+        wheN(duncan).followsTheLink("More information...", "http://www.iana.org/domains/example")
+        then(duncan).shouldSee(::`the page location`, equalTo(URI("http://www.iana.org/domains/reserved")))
+    }
+
+    @Test fun `Dispensing with the given when then`() {
+        duncan.he.loadsThePageAt("http://example.com")
+        duncan.he.followsTheLink("More information...", "http://www.iana.org/domains/example")
+        duncan.shouldSee(::`the page location`, equalTo(URI("http://www.iana.org/domains/reserved")))
+    }
+
 }
