fb692ef18d5fbe96c398a60e877aeeec17fdc0fd
Duncan McGregor <duncan@oneeyedmen.com>
2016-05-11 09:38:56 +0100

    More work on Konsent test runner

--- src/main/java/com/oneeyedmen/konsent/Konsent.kt
+++ src/main/java/com/oneeyedmen/konsent/Konsent.kt
@@ -2,12 +2,20 @@ package com.oneeyedmen.konsent
 
 import org.junit.runners.BlockJUnit4ClassRunner
 import org.junit.runners.model.FrameworkMethod
+import java.lang.reflect.Method
 
 class Konsent(klass: Class<*>) : BlockJUnit4ClassRunner(klass) {
 
     protected override fun computeTestMethods(): List<FrameworkMethod> {
         return testClass.getAnnotatedMethods(Scenario::class.java)
             .sortedBy { it.getAnnotation(Scenario::class.java).index }
+            .map { it.butNamed(it.getAnnotation(Scenario::class.java).name)}
     }
 
+}
+
+fun FrameworkMethod.butNamed(newName: String): FrameworkMethod  = RenamedFrameworkMethod(this.method, newName)
+
+class RenamedFrameworkMethod(method: Method, private val newName: String) : FrameworkMethod(method) {
+    override fun getName(): String = newName
 }
---
+++ src/test/java/com/oneeyedmen/konsent/TestRunnerNamingTests.kt
@@ -0,0 +1,22 @@
+package com.oneeyedmen.konsent
+
+import org.junit.runner.RunWith
+
+
+@RunWith(Konsent::class)
+class TestRunnerNamingTests : AcceptanceTest() {
+
+    @Scenario(name="number 2", index = 1) fun second() {
+    }
+
+    @Scenario(name="1", index = 0) fun first() {
+    }
+
+    @Scenario(name="erm, 3", index = 2) fun third() {
+    }
+
+    @Scenario(name="the fouth", index = 3) fun aaa() {
+    }
+
+}
+
