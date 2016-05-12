a3e411e723bf3614427346e88c85e4b68df1dad3
Duncan McGregor <duncan@oneeyedmen.com>
2016-05-12 09:12:19 +0100

    Working test runner implementation

--- src/main/java/com/oneeyedmen/konsent/Konsent.kt
+++ src/main/java/com/oneeyedmen/konsent/Konsent.kt
@@ -8,13 +8,12 @@ import org.junit.runner.Description
 import org.junit.runner.notification.RunNotifier
 import org.junit.runners.BlockJUnit4ClassRunner
 import org.junit.runners.model.FrameworkMethod
-import java.lang.reflect.Method
 
 class Konsent(klass: Class<*>) : BlockJUnit4ClassRunner(klass) {
 
     private val featureRule = MyFeatureRule(ApprovalsRule.usualRule())
 
-    protected override fun computeTestMethods(): List<FrameworkMethod> {
+    override fun computeTestMethods(): List<FrameworkMethod> {
         return testClass.getAnnotatedMethods(Scenario::class.java)
             .map { it.butNamed(it.getAnnotation(Scenario::class.java).name)}
             .sortedBy { it.getAnnotation(Scenario::class.java).index }
@@ -24,20 +23,22 @@ class Konsent(klass: Class<*>) : BlockJUnit4ClassRunner(klass) {
         super.run(notifier)
     }
 
+    // we fake featureRule attached to class
     override fun classRules(): List<TestRule> {
         return super.classRules().plus(featureRule)
     }
 
+    // and scenarioRule attached to instance
     override fun getTestRules(target: Any): List<TestRule> {
-        return super.getTestRules(target).plus(MyScenarioRule(featureRule.recorder))
+        val element: MyScenarioRule = MyScenarioRule(target, featureRule.recorder)
+        return super.getTestRules(target).plus(element)
     }
 }
 
-private fun FrameworkMethod.butNamed(newName: String): FrameworkMethod = if (newName == "") this else RenamedFrameworkMethod(this.method, newName)
-
-private class RenamedFrameworkMethod(method: Method, private val newName: String) : FrameworkMethod(method) {
-    override fun getName(): String = newName
-}
+private fun FrameworkMethod.butNamed(newName: String) = if (newName == "") this
+    else object: FrameworkMethod(method) {
+        override fun getName(): String = newName
+    }
 
 class MyFeatureRule(private val approvalsRule: ApprovalsRule) : TestWatcher() {
 
@@ -47,25 +48,31 @@ class MyFeatureRule(private val approvalsRule: ApprovalsRule) : TestWatcher() {
         approvalsRule.starting(description)
         recorder = TranscriptFeatureRecorder(approvalsRule.transcript())
         recorder.featureStart(nameFromClassName(description.testClass), *preambleFor(description.testClass))
+        injectRecorder(description.testClass, null, recorder)
     }
 
     private fun preambleFor(testClass: Class<*>): Array<String> {
         val preamble = testClass.getAnnotation(Preamblex::class.java)
-        return listOf(preamble.p1, preamble.p2, preamble.p3).filter { it.isNotBlank() }.toTypedArray()
+        return if (preamble != null) listOf(preamble.p1, preamble.p2, preamble.p3).filter { it.isNotBlank() }.toTypedArray()
+        else emptyArray()
     }
 
     override fun succeeded(description: Description) {
         approvalsRule.succeeded(description)
     }
 
+    private val spaceOutCamelCase = Regex("(\\p{Ll})(\\p{Lu})")
+    private fun String.`space Out Camel Case`() = this.replace(spaceOutCamelCase, "$1 $2")
+    private fun nameFromClassName(clazz: Class<*>) = clazz.simpleName.`space Out Camel Case`()
 }
 
-class MyScenarioRule(val recorder: FeatureRecorder) : TestWatcher() {
+class MyScenarioRule(private val target: Any, private val recorder: FeatureRecorder) : TestWatcher() {
     override fun starting(description: Description) {
+        injectRecorder(target.javaClass, target, recorder)
         recorder.scenarioStart(description.methodName)
     }
 }
 
-private val spaceOutCamelCase = Regex("(\\p{Ll})(\\p{Lu})")
-private fun String.`space Out Camel Case`() = this.replace(spaceOutCamelCase, "$1 $2")
-private fun nameFromClassName(instance: Any) = instance.javaClass.simpleName.`space Out Camel Case`()
+private fun <T> injectRecorder(clazz: Class<T>, target: T?, recorder: FeatureRecorder) {
+    clazz.fields.filter { it.type == FeatureRecorder::class.java }.forEach { it.set(target, recorder) }
+}
---
+++ src/test/java/com/oneeyedmen/konsent/InjectsRecorderTest.kt
@@ -0,0 +1,21 @@
+package com.oneeyedmen.konsent
+
+import org.junit.Assert.assertNotNull
+import org.junit.runner.RunWith
+
+
+@RunWith(Konsent::class)
+class InjectsRecorderTest {
+
+    companion object {
+        lateinit var recorder: FeatureRecorder
+    }
+
+    val recorderDuringConstruction  = recorder
+
+    @Scenario(index = 0) fun first() {
+        assertNotNull(recorder)
+        assertNotNull(recorderDuringConstruction)
+    }
+}
+
